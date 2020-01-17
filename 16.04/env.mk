9414719c72846ef5d8af00b46d7deab16bdf7a1d
Jérôme Tamba <jtamba@online.net>
2020-01-17 11:03:10 +0100

    Bump xenial bootscript version on arm64

--- 16.04/env.mk
+++ 16.04/env.mk
@@ -5,5 +5,6 @@ IMAGE_TITLE = Ubuntu Xenial
 IMAGE_DESCRIPTION = Ubuntu Xenial (16.04 LTS)
 IMAGE_SOURCE_URL = https://github.com/scaleway/image-ubuntu
 IMAGE_VENDOR_URL = http://www.ubuntu.com
+IMAGE_BOOTSCRIPT_arm64 = longterm 4.9 latest
 IMAGE_BOOTSCRIPT = longterm 4.4 latest
 IMAGE_BASE_FLAVORS = common docker-based systemd feature-motd
